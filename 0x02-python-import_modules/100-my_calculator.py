57d8cdaac7faa60f8e36e5102d59fcdc778176c3
iamprincetj <tochukwunwanze5@gmail.com>
2023-03-09 07:21:13 -0800

    100-my_calculator.py, alx task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
-from sys import argv, exit
+    from sys import argv, exit
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
