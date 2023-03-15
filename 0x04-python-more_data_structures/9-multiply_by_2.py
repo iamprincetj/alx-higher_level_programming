f04df5b714d12b13b0f44fbb397d992f48791530
iamprincetj <tochukwunwanze5@gmail.com>
2023-03-15 09:27:24 -0700

    9-multiply_by_2.py, alx task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    dict_key = a_dictionary.keys()
-    new_dict = {}
-    for i in dict_key:
-        new_dict.append(a_dictionary.get(i) * 2)
-    return new_dict
+    return {x: y*2 for x, y in a_dictionary.items()}
