49582bfd91c89ad1207bbdb7a61ee41d636bfd9c
iamprincetj <tochukwunwanze5@gmail.com>
2023-03-27 12:03:39 -0700

    102-magic_calculation.py, alx task

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,6 +9,6 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except:
-            result = b + a
+            result = a + b
             break
     return result
