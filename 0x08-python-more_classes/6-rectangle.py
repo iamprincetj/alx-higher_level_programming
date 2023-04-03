b1a83a387bf3a61b71f298099c1fee61a224cc61
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-03 19:07:32 +0000

    corrected it

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
          Return: the string of rectange
     '''
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
