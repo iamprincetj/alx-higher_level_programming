3aa925fad3b905a0c655a3190f26fe0a292babb2
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-03 23:01:43 +0000

    done

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''solves the n queen puzzle'''
+import sys
+
+
+def nqueens(n):
+    if isinstance(n, int) == True:
+        print("N must be a number")
+        sys.exit(1)
+    n = int(n)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    def is_valid(board, ro, col):
+        for r, c in board:
+            if r == ro or c == col or r - c == ro - col or r + c == ro + col:
+                return False
+        return True
+
+    def backtrack(board, row):
+        if row == n:
+            print(board)
+            return
+        for col in range(n):
+            if is_valid(board, row, col):
+                board.append([row, col])
+                backtrack(board, row + 1)
+                board.pop()
+
+    backtrack([], 0)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    nqueens(sys.argv[1])
