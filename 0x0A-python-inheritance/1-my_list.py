9edac758027bb805da53492266791e3de54daef5
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-11 14:25:37 +0000

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/in/python3
+'''A module that contain a class (MyList) which inherits from
+   the class (List)
+'''
+
+
+class MyList(list):
+    '''The class MyList inheriting from list
+    '''
+    def print_sorted(self):
+        print(sorted(self))
