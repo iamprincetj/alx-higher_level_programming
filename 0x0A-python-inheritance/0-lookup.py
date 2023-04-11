affa6de7eca6edd18c3cc6ecfac8d00040628f75
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-11 13:45:18 +0000

    My first commit of the day

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''A module that contains a class called lookup'''
+
+def lookup(obj):
+    ''' The class  that returns the list of available
+        attributes and methods of an object
+    Arguments:
+        obj: the object to check
+    Return: a list object
+    '''
+
+    return dir(obj)
