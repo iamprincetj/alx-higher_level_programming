14378005499d56552f10443acdf090d34f86281e
iamprincetj <tochukwunwanze5@gmail.com>
2023-04-12 16:31:38 +0000

    corrected

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
         return []
 
     my_list = [[1]]
-    for i in range(1, n): 
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(my_list[i-1][j-1] + my_list[i-1][j])
