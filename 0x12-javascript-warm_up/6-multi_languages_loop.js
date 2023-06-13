70dd22a443fd523a21f02fa8ee4d9db10a0ab18e
iamprincetj <tochukwunwanze5@gmail.com>
2023-06-13 12:04:27 +0000

    6-multi_languages_loop.js updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(line);
+//for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  //console.log(line);
+//}
+
+const num = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+for (const line of num) {
+	console.log(line);
 }
