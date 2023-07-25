aee5f599448d8f80091f39609a7c1c3ecfd586bd
iamprincetj <tochukwunwanze5@gmail.com>
2023-07-25 23:55:23 +0000

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+  if (!error) {
     const todos = JSON.parse(body);
     const completed = {};
     todos.forEach((todo) => {
-      if (todo.completed) {
-        if (completed[todo.userId] === undefined) {
-          completed[todo.userId] = 1;
-        } else {
-          completed[todo.userId] += 1;
-        }
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
     console.log(completed);
