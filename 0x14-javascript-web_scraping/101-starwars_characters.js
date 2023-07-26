c33db96bb81d3bc5e0e3e5c4662f7074d6c8c747
iamprincetj <tochukwunwanze5@gmail.com>
2023-07-26 18:18:38 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
+const url = 'https://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const charactersUrls = JSON.parse(body).characters;
-    charactersUrls.forEach((characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
